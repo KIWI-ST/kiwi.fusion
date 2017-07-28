8519ef1a629ca8e52965fa9c5f57df4c11f29ef7
axmand <815966508@qq.com>
2017-07-28 23:58:03 +0800

    Initial Commit

---
+++ src/gl/buffer/GLVertexbuffer.js
@@ -0,0 +1,18 @@
+/**
+ * @class
+ */
+const GLBuffer = require('./GLBuffer');
+
+class GLVertexbuffer extends GLBuffer {
+    /**
+     * 
+     * @param {WebGLRenderingContext} gl 
+     * @param {ArrayBuffer| SharedArrayBuffer|ArrayBufferView} data 
+     * @param {gl.STATIC_DRAW|gl.DYNAMIC_DRAW|gl.STREAM_DRAW} drawType 
+     */
+    constructor(gl, data, drawType) {
+        super(gl, GLConstants.ARRAY_BUFFER, data, drawType);
+    }
+}
+
+module.exports = GLVertexbuffer;
---
+++ src/init.js
@@ -0,0 +1,5 @@
+const GLBuffer = require('./gl/buffer/GLBuffer'),
+    GLIndexBuffer=require('./gl/buffer/GLIndexBuffer'),
+    GLVertexBuffer=require('./gl/buffer/GLVertexBuffer');
+
+const sss=new GLIndexBuffer();
