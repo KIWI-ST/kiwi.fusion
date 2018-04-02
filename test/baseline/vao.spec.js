d9ea62286252d2fa673f07eb540a343b095f8100
axmand <815966508@qq.com>
2018-04-02 21:44:32 +0800

    fix

--- test/baseline/vao.spec.js
+++ test/baseline/vao.spec.js
@@ -8,8 +8,22 @@ const assert = require('assert'),
     kiwi = require('./../../src/init');
 
 const glCavnas = new kiwi.gl.GLCanvas("mapCanvas");
+/**
+ * @type {WebGLRenderingContext}
+ */
 const gl = glCavnas.getContext('webgl');
 const ext = gl.getExtension('OES_vertex_array_object');
+const vao = ext.createVertexArrayOES();
+ext.bindVertexArrayOES(vao);
+/**
+ * https://github.com/greggman/webgl2-fundamentals/blob/87438d6e185379e06c01e1f7f11fe78fee207f78/webgl/webgl-clipspace-rectangles.html
+ */
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
+gl.bufferData(gl.ARRAY_BUFFER,new Float32Array([0,0,0]),gl.STATIC_DRAW);
+
+
+
 
 // describe('baseline test', () => {
 //     it('#1.vertex array object', () => {
