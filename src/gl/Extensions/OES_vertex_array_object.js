4059b25627ea894f8432820857f5e86e06d3388f
axmand <815966508@qq.com>
2018-04-03 17:28:34 +0800

    fix

--- src/gl/Extensions/OES_vertex_array_object.js
+++ src/gl/Extensions/OES_vertex_array_object.js
@@ -56,12 +56,150 @@ class OES_vertex_array_object extends Extension {
      * @param {String} extName 
      * @param {GLContext} glContext 
      */
-    constructor(extName,glContext) {
-        super(extName,glContext);
+    constructor(extName, glContext) {
         /**
-         * @type {Int}
+         * 
+         */
+        super(extName, glContext);
+        /**
+         * self object
+         */
+        const self = this;
+        /**
+         * @type {number}
          */
         this.VERTEX_ARRAY_BINDING_OES = GLConstants.VERTEX_ARRAY_BINDING_OES;
+        /**
+         * @type {object}
+         */
+        this.original = {
+            getParameter: this._glContext.getParameter,
+            enableVertexAttribArray: this._glContext.enableVertexAttribArray,
+            disableVertexAttribArray: this._glContext.disableVertexAttribArray,
+            bindBuffer: this._glContext.bindBuffer,
+            getVertexAttrib: this._glContext.getVertexAttrib,
+            vertexAttribPointer: this._glContext.vertexAttribPointer
+        };
+        /**
+         * overwrite
+         */
+        this._glContext.getParameter = function(pName){
+            if(pName===GLConstants.VERTEX_ARRAY_BINDING_OES){
+                if (self.currentVertexArrayObject == self.defaultVertexArrayObject) {
+                    return null;
+                } else {
+                    return self.currentVertexArrayObject;
+                }
+            }
+            return self.original.getParameter.apply(this, arguments);
+        };
+        /**
+         * 
+         */
+        this._glContext.enableVertexAttribArray = function(pName){
+            var vao = self.currentVertexArrayObject;
+            vao.maxAttrib = Math.max(vao.maxAttrib, index);
+            var attrib = vao.attribs[index];
+            attrib.enabled = true;
+            return original.enableVertexAttribArray.apply(this, arguments);
+        }
+        /**
+         * 
+         * @param {*} index 
+         */
+        this._glContext.disableVertexAttribArray = function disableVertexAttribArray(index) {
+            var vao = self.currentVertexArrayObject;
+            vao.maxAttrib = Math.max(vao.maxAttrib, index);
+            var attrib = vao.attribs[index];
+            attrib.enabled = false;
+            return original.disableVertexAttribArray.apply(this, arguments);
+        };
+        /**
+         * @param
+         */
+        this._glContext.bindBuffer = function bindBuffer(target, buffer) {
+            switch (target) {
+                case gl.ARRAY_BUFFER:
+                    self.currentArrayBuffer = buffer;
+                    break;
+                case gl.ELEMENT_ARRAY_BUFFER:
+                    self.currentVertexArrayObject.elementArrayBuffer = buffer;
+                    break;
+            }
+            return self.original.bindBuffer.apply(this, arguments);
+        };
+        /**
+         * 
+         * @param {*} index 
+         * @param {*} pname 
+         */
+        this._glContext.getVertexAttrib = function getVertexAttrib(index, pname) {
+            var vao = self.currentVertexArrayObject;
+            var attrib = vao.attribs[index];
+            switch (pname) {
+                case gl.VERTEX_ATTRIB_ARRAY_BUFFER_BINDING:
+                    return attrib.buffer;
+                case gl.VERTEX_ATTRIB_ARRAY_ENABLED:
+                    return attrib.enabled;
+                case gl.VERTEX_ATTRIB_ARRAY_SIZE:
+                    return attrib.size;
+                case gl.VERTEX_ATTRIB_ARRAY_STRIDE:
+                    return attrib.stride;
+                case gl.VERTEX_ATTRIB_ARRAY_TYPE:
+                    return attrib.type;
+                case gl.VERTEX_ATTRIB_ARRAY_NORMALIZED:
+                    return attrib.normalized;
+                default:
+                    return original.getVertexAttrib.apply(this, arguments);
+            }
+        };
+        /**
+         * 
+         * @param {*} indx 
+         * @param {*} size 
+         * @param {*} type 
+         * @param {*} normalized 
+         * @param {*} stride 
+         * @param {*} offset 
+         */
+        this._glContext.vertexAttribPointer = function vertexAttribPointer(indx, size, type, normalized, stride, offset) {
+            var vao = self.currentVertexArrayObject;
+            vao.maxAttrib = Math.max(vao.maxAttrib, indx);
+            var attrib = vao.attribs[indx];
+            attrib.buffer = self.currentArrayBuffer;
+            attrib.size = size;
+            attrib.type = type;
+            attrib.normalized = normalized;
+            attrib.stride = stride;
+            attrib.offset = offset;
+            attrib.recache();
+            return original.vertexAttribPointer.apply(this, arguments);
+        };
+        /**
+         * 
+         */
+        if (this._glContext.instrumentExtension) {
+            gl.instrumentExtension(this, "OES_vertex_array_object");
+        }
+        /**
+         * 
+         */
+        gl.canvas.addEventListener('webglcontextrestored', function() {
+            log("OESVertexArrayObject emulation library context restored");
+            self.reset_();
+        }, true);
+        /**
+         * reset all default vertex array object
+         */
+        this._reset();
+    }
+    /**
+     * reset
+     * -vertexArrayObjects
+     * -defaultVertexArrayObject
+     * -currentVertexArrayObject
+     */
+    _reset() {
         /**
          * @type {WebGLVertexArrayObjectOES}
          */
@@ -74,7 +212,6 @@ class OES_vertex_array_object extends Extension {
          * @type {WebGLVertexArrayObjectOES}
          */
         this.currentVertexArrayObject;
-
     }
     /**
      * create WebGLVertexArrayObjectOES
@@ -95,6 +232,16 @@ class OES_vertex_array_object extends Extension {
         this.currentVertexArrayObject == arrayObject ? this.bindVertexArrayOES(null) : null;
     }
     /**
+     * infer object
+     * @param {WebGLVertexArrayObjectOES} arrayObject 
+     */
+    isVertexArrayOES(arrayObject) {
+        if (arrayObject && arrayObject instanceof WebGLVertexArrayObjectOES)
+            if (arrayObject.hasBeenBound && arrayObject._ext == this)
+                return true;
+        return false;
+    }
+    /**
      * 
      * @param {WebGLVertexArrayObjectOES} arrayObject 
      */
@@ -110,12 +257,12 @@ class OES_vertex_array_object extends Extension {
         this.currentVertexArrayObject.hasBeenBound = true;
         const newVao = this.currentVertexArrayObject;
         //
-        if(oldVao === newVao){
+        if (oldVao === newVao) {
             return;
         }
         //
-        if(!oldVao||newVao.elementArrayBuffer!=oldVao.elementArrayBuffer){
-            
+        if (!oldVao || newVao.elementArrayBuffer != oldVao.elementArrayBuffer) {
+
         }
 
 
--- test/baseline/vao.spec.js
+++ test/baseline/vao.spec.js
@@ -16,11 +16,19 @@ const ext = gl.getExtension('OES_vertex_array_object');
 const vao = ext.createVertexArrayOES();
 ext.bindVertexArrayOES(vao);
 /**
+ * https://segmentfault.com/a/1190000012174165
+ * https://webgl2fundamentals.org/webgl/lessons/webgl1-to-webgl2.html
  * https://github.com/greggman/webgl2-fundamentals/blob/87438d6e185379e06c01e1f7f11fe78fee207f78/webgl/webgl-clipspace-rectangles.html
  */
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
+
+const positions = new Float32Array([
+    -0.5, -0.5, 0.0,0.0, -0.5, 0.0,0.0, 0.0, 0.0
+]);
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER,new Float32Array([0,0,0]),gl.STATIC_DRAW);
+gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
 
 
 
